18762f328ab69b200f7735175f4bace9bd6b776e
J-Ellette <james.ellette@gmail.com>
2025-11-05 01:51:31 -0600

    Delete modules_to_convert directory

--- modules_to_convert/modules/common/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Common utilities for CIV-ARCOS code scanning modules.
-"""
-
-from .base_scanner import BaseScanner
-from .report_generator import ReportGenerator
-from .grading import GradingSystem
-
-__all__ = ['BaseScanner', 'ReportGenerator', 'GradingSystem']
--- modules_to_convert/modules/headless_drakon/index.ts
+++
@@ -1,3 +0,0 @@
-export { DrakonGraphBuilder } from './DrakonGraphBuilder';
-export { DrakonNodeType } from './types';
-export type { DrakonNode, DrakonConnection, DrakonGraph } from './types';
